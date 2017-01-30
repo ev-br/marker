cfd4d6d92332b869ab30258b8552427e92cd765e
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2017-01-31 02:23:32 +0300

    MAINT: trivial refactor LMSzip

--- LMSzip.py
+++ LMSzip.py
@@ -1,41 +1,28 @@
+from __future__ import division, print_function, absolute_import
 import zipfile
-from zipfile import ZipFile
 import os
-import shutil
 
-d = {} # словарь
-f = open('text.txt', 'r', encoding='utf8')
-
-# Заполнение словаря
-for line in f:
-    k = line.replace("\n","").split(None, 1)
-    d[k[0]] = k[1]
-
-f.close()
-
-print(d)
-
+# Заполнение словаря имен
+d = {}
+with open('text.txt', 'r', encoding='utf8') as f: 
+    for line in f:
+        k = line.replace("\n","").split(None, 1)
+        d[k[0]] = k[1]
 
 #fname = 'ex1/zadanie-1---mashinnaya-arifmetika_20170130-235632.zip'
 fname = 'ex2/zadanie-2---kvadratnoe-uravnenie_20170131-002923.zip'
 path, fname = os.path.split(fname)
 os.chdir(path)
 
-###import pdb; pdb.set_trace()
-
 # Проверка на zip
 print(zipfile.is_zipfile(fname))
 
 # Открыть архив для чтения и распоковать
-z = ZipFile(fname, 'r')
-z.extractall()
-z.close()
+with zipfile.ZipFile(fname, 'r') as z:
+    z.extractall()
 
-#Список имен файлов
+# Список имен файлов
 sps = os.listdir(path=os.getcwd())
-
-#Смена директории
-###os.chdir(fname)
 
 for i in range (len(sps)):
     # Проверка является ли файл .py или .ipynb
@@ -45,6 +32,4 @@ for i in range (len(sps)):
                 dirnew = os.getcwd() + '/' + str(key) + '/' + sps[i]
                 dirold = os.getcwd() + '/' + sps[i]
                 os.renames(dirold, dirnew)
-
-                
 
