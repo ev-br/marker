8ab7dd53a8fb20f86817f61b22fb92a027af3c2e
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2017-01-31 02:38:22 +0300

    MAINT: refactor LMSzip some more

--- LMSzip.py
+++ LMSzip.py
@@ -1,35 +1,52 @@
 from __future__ import division, print_function, absolute_import
+import argparse
 import zipfile
 import os
 
-# Заполнение словаря имен
-d = {}
-with open('text.txt', 'r', encoding='utf8') as f: 
-    for line in f:
-        k = line.replace("\n","").split(None, 1)
-        d[k[0]] = k[1]
+def fill_namedict(fname='text.txt'):
+    # Заполнение словаря имен
+    d = {}
+    with open(fname, 'r', encoding='utf8') as f: 
+        for line in f:
+            k = line.replace("\n","").split(None, 1)
+            d[k[0]] = k[1]
+    return d
 
-#fname = 'ex1/zadanie-1---mashinnaya-arifmetika_20170130-235632.zip'
-fname = 'ex2/zadanie-2---kvadratnoe-uravnenie_20170131-002923.zip'
-path, fname = os.path.split(fname)
-os.chdir(path)
 
-# Проверка на zip
-print(zipfile.is_zipfile(fname))
+def unpack(zip_fname, name_dict):
+    """Unpack the zip archive into per-student folders
+    """
 
-# Открыть архив для чтения и распоковать
-with zipfile.ZipFile(fname, 'r') as z:
-    z.extractall()
+    # XXX: relative paths etc
+    path, fname = os.path.split(zip_fname)
+    os.chdir(path)
 
-# Список имен файлов
-sps = os.listdir(path=os.getcwd())
+    if not zipfile.is_zipfile(fname):
+        raise ValueError("%s is not a zip file" % fname)
 
-for i in range (len(sps)):
-    # Проверка является ли файл .py или .ipynb
-    if (sps[i].find(".py") != -1)or(sps[i].find(".ipynb") != -1):
-        for key in d:
-            if (sps[i].find(key) != -1):
-                dirnew = os.getcwd() + '/' + str(key) + '/' + sps[i]
-                dirold = os.getcwd() + '/' + sps[i]
-                os.renames(dirold, dirnew)
+    # Открыть архив для чтения и распоковать
+    with zipfile.ZipFile(fname, 'r') as z:
+        z.extractall()
 
+    # Список имен файлов
+    sps = os.listdir(path=os.getcwd())
+
+    for fname in sps:
+        # Проверка является ли файл .py или .ipynb
+        if (fname.find(".py") != -1)or(fname.find(".ipynb") != -1):
+            for key in name_dict:
+                if key in fname:
+                    dirnew = os.path.join(os.getcwd(), str(key), fname)
+                    dirold = os.path.join(os.getcwd(), fname)
+                    os.renames(dirold, dirnew)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path",
+                        help="Path to the zip file from LMS.")
+    args = parser.parse_args()
+
+    # fill the name map & unpack
+    d = fill_namedict()
+    unpack(args.path, d)
